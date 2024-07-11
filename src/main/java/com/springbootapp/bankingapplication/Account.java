079d6a408423da24a263740c52c961c14f5649c3
Deroyal1 <benjaminokwori@gmail.com>
2024-07-11 23:19:37 +0100

    dockerfile update

--- src/main/java/com/springbootapp/bankingapplication/Account.java
+++ src/main/java/com/springbootapp/bankingapplication/Account.java
@@ -1,2 +1,28 @@
-package com.springbootapp.bankingapplication;public class Account {
+package com.springbootapp.bankingapplication;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Getter
+@Setter
+@Entity
+@EnableWebMvc
+
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String accountNumber;
+    private String accountType;
+    private String accountStatus;
+    private String customerAddress;
+    private String customerName;
+    private double balance;
 }
--- src/main/java/com/springbootapp/bankingapplication/AccountService.java
+++ src/main/java/com/springbootapp/bankingapplication/AccountService.java
@@ -1,2 +1,46 @@
-package com.springbootapp.bankingapplication;public class AccountService {
+package com.springbootapp.bankingapplication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.Optional;
+
+@Configuration
+@EnableWebMvc
+@Service
+public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public Account createAccount(Account account){
+        return accountRepository.save(account);
+    }
+
+    public Optional<Account> getAccount(Long id) {
+        return accountRepository.findById(id);
+    }
+
+    public Account deposit(Long id, double amount) {
+        Account account = getAccount(id).orElseThrow(() -> new RuntimeException("Account not found"));
+        account.setBalance(account.getBalance() + amount);
+        return accountRepository.save(account);
+    }
+
+    public Account withdraw(Long id, double amount) {
+        Account account = getAccount(id).orElseThrow(() -> new RuntimeException("Account not found"));
+        if (account.getBalance() < amount) {
+            throw new RuntimeException("Insufficient funds");
+        }
+        account.setBalance(account.getBalance() - amount);
+        return accountRepository.save(account);
+    }
+
+    public static interface AccountRepository extends JpaRepository<Account, Long> {
+    }
 }
+
